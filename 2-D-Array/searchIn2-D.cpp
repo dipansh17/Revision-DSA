f415f8fde7ed0fd5ef814be01722d039a708905f
dipansh17 <70329771+dipansh17@users.noreply.github.com>
2022-11-05 21:59:45 +0530

    ReCommit all files

---
+++ 2-D-Array/searchIn2-D.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+pair<int,int> search(int arr[][3],int n,int target){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i][j]==target){
+                return {i,j};
+            }
+        }
+    }
+    return {-1,-1};
+}
+int main(){
+    int arr[3][3]={1,2,3,4,5,6,7,8,9};
+    int target=5;
+    int n=3;
+    pair<int,int>p=search(arr,n,target);
+    cout<<p.first<<" "<< p.second<<endl;
+    return 0;
+}
